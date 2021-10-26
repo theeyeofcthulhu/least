0b90070f9fe3e9c1e080b33ff4634e5723d17a24
Julian Eigmüller <j.eigmueller17@gmail.com>
2021-10-26 21:56:19 +0200

    add emacs least-mode

---
+++ editors/least-mode.el
@@ -0,0 +1,26 @@
+;;; least-mode.el --- Major Mode for editing Least programs code -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Major Mode for editing Least programs code.
+;;; Code:
+
+(defvar least-mode-hook nil)
+
+(eval-and-compile
+  (defconst least-keywords
+    '("if" "end" "print" "exit" "int")))
+
+(defconst least-highlights
+  `((,(regexp-opt least-keywords 'symbols) . font-lock-keyword-face)))
+
+;;;###autoload
+(define-derived-mode least-mode prog-mode "least"
+  "Major Mode for editing Least source code."
+  (setq font-lock-defaults '(least-highlights)))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.least\\'" . least-mode))
+
+(provide 'least-mode)
+
+;;; least-mode.el ends here
