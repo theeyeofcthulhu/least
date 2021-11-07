5588bded3bad993fc0a96ec985cabfabacccf905
Julian Eigmüller <j.eigmueller17@gmail.com>
2021-11-07 23:17:59 +0100

    Don't expose compiler_error_core

--- error.h
+++ error.h
@@ -7,6 +7,5 @@
 void compiler_error_on_false(bool eval, char* source_file, int line, char* format, ...);
 void compiler_error_on_true(bool eval, char* source_file, int line, char* format, ...);
 void compiler_error(char* source_file, int line, char* format, ...);
-void compiler_error_core(char* source_file, int line, char* format, va_list format_list);
 
 #endif // ERROR_H_
